ea0d9d095e211ee681338a9f4f285a67fa2280f4
Manoel <manoelivisson4@hotmail.com>
2025-02-25 14:21:20 -0300

    Funções do meio somador terminadas e inicio do somador completo

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 function portaAND(valor1, valor2) {
-    if (valor1 && valor2) {
+    if (valor1 == 1 && valor2 == 1) {
+        console.log("Ioiosdasdf")
         return 1;
     } else {
         return 0;
@@ -14,8 +15,29 @@ function portaXOR(valor1, valor2) {
     }
 }
 
+function meioSomador(valor1, valor2) {
+    let soma, carry = 0;
+
+    soma = portaXOR(valor1, valor2);
+    carry = portaAND(valor1, valor2);
+    console.log(carry)
+
+    return [soma, carry];
+}
+
+function limpaResultados() {
+    document.getElementById('soma').textContent = "Soma = ";
+    document.getElementById('carry').textContent = "Carry = ";
+}
+
 function calcula() {
-    let a = document.getElementById('entradaA').value
-    let b = document.getElementById('entradaB').value
-    document.getElementById('res').value = portaXOR(a, b);
+    limpaResultados();
+    let a = document.getElementById('entradaA').value;
+    let b = document.getElementById('entradaB').value;
+
+    let soma = meioSomador(a, b)[0];
+    let carry = meioSomador(a, b)[1];
+
+    document.getElementById('soma').textContent += soma;
+    document.getElementById('carry').textContent += carry;
 }

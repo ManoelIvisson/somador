f4adcdc77d2929a953e622e6fca7ef8f468658ab
André <andregsdantas@gmail.com>
2025-03-07 17:06:50 -0300

    somador de 4 bits versão raio x está funcional

--- js.js
+++ js.js
@@ -272,7 +272,7 @@ class portaAndRaioX {
 };
 
 class portaOr3RaioX {
-    constructor (entradas, saida,  led=false) {
+    constructor (entradas, saida, led=false) {
         this.entradas = entradas;
         this.saida = saida;
         this.portas_afetadas = [];
@@ -290,12 +290,18 @@ class portaOr3RaioX {
         if (this.entradas[0].estado || this.entradas[1].estado || this.entradas[2].estado) {
             this.estado = true;
             document.getElementById(this.saida).style.opacity="100%";
+            if (this.led[0]) {
+                document.getElementById(this.led[1]).src = this.led[2];
+            }
             this.portas_afetadas.forEach(porta => {
                 porta.mudaEstado()
             });
         } else {
             this.estado = false;
             document.getElementById(this.saida).style.opacity="50%";
+            if (this.led[0]) {
+                document.getElementById(this.led[1]).src = this.led[3];
+            }
             this.portas_afetadas.forEach(porta => {
                 porta.mudaEstado()
             });
@@ -345,21 +351,93 @@ botaoA1 = new BotaoRaioX("botao-a1", "fio-botao-a1");
 botaoB1 = new BotaoRaioX("botao-b1", "fio-botao-b1");
 botaoCarry = new BotaoRaioX("botao-carry", "fio-botao-carry");
 
+botaoA2 = new BotaoRaioX("botao-a2", "fio-botao-a2");
+botaoB2 = new BotaoRaioX("botao-b2", "fio-botao-b2");
+
+botaoA3 = new BotaoRaioX("botao-a3", "fio-botao-a3");
+botaoB3 = new BotaoRaioX("botao-b3", "fio-botao-b3");
+
+botaoA4 = new BotaoRaioX("botao-a4", "fio-botao-a4");
+botaoB4 = new BotaoRaioX("botao-b4", "fio-botao-b4");
+
+
+
 portaAndCima1 = new portaAndRaioX([botaoB1, botaoCarry], "saida-and-cima1");
 portaAndMeio1 = new portaAndRaioX([botaoA1, botaoB1], "saida-and-meio1");
 portaAndBaixo1 = new portaAndRaioX([botaoA1, botaoCarry], "saida-and-baixo1");
+portaOr1 = new portaOr3RaioX ([portaAndCima1, portaAndMeio1, portaAndBaixo1], "carry-entrada2");
 
-portaOr1 = new portaOr3RaioX ([portaAndCima1, portaAndMeio1, portaAndBaixo1], "carry-entrada2")
+portaAndCima2 = new portaAndRaioX([botaoB2, portaOr1], "saida-and-cima2");
+portaAndMeio2 = new portaAndRaioX([botaoA2, botaoB2], "saida-and-meio2");
+portaAndBaixo2 = new portaAndRaioX([botaoA2, portaOr1], "saida-and-baixo2");
+portaOr2 = new portaOr3RaioX ([portaAndCima2, portaAndMeio2, portaAndBaixo2], "carry-entrada3");
 
-portaXorBaixo1 = new portaXorRaioX([botaoB1, botaoCarry], "saida-xor1")
-portaXorCima1 = new portaXorRaioX([botaoA1, portaXorBaixo1], "saida1", [true, "led1", "img/led1-on.png", "img/led1-off.png"])
+portaAndCima3 = new portaAndRaioX([botaoB3, portaOr2], "saida-and-cima3");
+portaAndMeio3 = new portaAndRaioX([botaoA3, botaoB3], "saida-and-meio3");
+portaAndBaixo3 = new portaAndRaioX([botaoA3, portaOr2], "saida-and-baixo3");
+portaOr3 = new portaOr3RaioX ([portaAndCima3, portaAndMeio3, portaAndBaixo3], "carry-entrada4");
+
+portaAndCima4 = new portaAndRaioX([botaoB4, portaOr3], "saida-and-cima4");
+portaAndMeio4 = new portaAndRaioX([botaoA4, botaoB4], "saida-and-meio4");
+portaAndBaixo4 = new portaAndRaioX([botaoA4, portaOr3], "saida-and-baixo4");
+portaOr4 = new portaOr3RaioX ([portaAndCima4, portaAndMeio4, portaAndBaixo4], "carry-saida", [true, "ledcarry", "img/led-carry-on.png", "img/led-carry-off.png"]);
+
+
+
+portaXorBaixo1 = new portaXorRaioX([botaoB1, botaoCarry], "saida-xor1");
+portaXorCima1 = new portaXorRaioX([botaoA1, portaXorBaixo1], "saida1", [true, "led1", "img/led1-on.png", "img/led1-off.png"]);
+
+portaXorBaixo2 = new portaXorRaioX([botaoB2, portaOr1], "saida-xor2");
+portaXorCima2 = new portaXorRaioX([botaoA2, portaXorBaixo2], "saida2", [true, "led2", "img/led2-on.png", "img/led2-off.png"]);
+
+portaXorBaixo3 = new portaXorRaioX([botaoB3, portaOr2], "saida-xor3");
+portaXorCima3 = new portaXorRaioX([botaoA3, portaXorBaixo3], "saida3", [true, "led3", "img/led3-on.png", "img/led3-off.png"]);
+
+portaXorBaixo4 = new portaXorRaioX([botaoB4, portaOr3], "saida-xor4");
+portaXorCima4 = new portaXorRaioX([botaoA4, portaXorBaixo4], "saida4", [true, "led4", "img/led4-on.png", "img/led4-off.png"]);
+
+
 
 botaoA1.addPortas([portaAndMeio1, portaAndBaixo1, portaXorCima1]);
 botaoB1.addPortas([portaAndCima1, portaAndMeio1, portaXorBaixo1]);
 botaoCarry.addPortas([portaAndCima1, portaAndBaixo1, portaXorBaixo1]);
 
-portaAndCima1.addPortas([portaOr1])
-portaAndMeio1.addPortas([portaOr1])
-portaAndBaixo1.addPortas([portaOr1])
+botaoA2.addPortas([portaAndMeio2, portaAndBaixo2, portaXorCima2]);
+botaoB2.addPortas([portaAndCima2, portaAndMeio2, portaXorBaixo2]);
+portaOr1.addPortas([portaAndCima2, portaAndBaixo2, portaXorBaixo2]);
 
-portaXorBaixo1.addPortas([portaXorCima1])
+botaoA3.addPortas([portaAndMeio3, portaAndBaixo3, portaXorCima3]);
+botaoB3.addPortas([portaAndCima3, portaAndMeio3, portaXorBaixo3]);
+portaOr2.addPortas([portaAndCima3, portaAndBaixo3, portaXorBaixo3]);
+
+botaoA4.addPortas([portaAndMeio4, portaAndBaixo4, portaXorCima4]);
+botaoB4.addPortas([portaAndCima4, portaAndMeio4, portaXorBaixo4]);
+portaOr3.addPortas([portaAndCima4, portaAndBaixo4, portaXorBaixo4]);
+
+
+
+portaAndCima1.addPortas([portaOr1]);
+portaAndMeio1.addPortas([portaOr1]);
+portaAndBaixo1.addPortas([portaOr1]);
+
+portaAndCima2.addPortas([portaOr2]);
+portaAndMeio2.addPortas([portaOr2]);
+portaAndBaixo2.addPortas([portaOr2]);
+
+portaAndCima3.addPortas([portaOr3]);
+portaAndMeio3.addPortas([portaOr3]);
+portaAndBaixo3.addPortas([portaOr3]);
+
+portaAndCima4.addPortas([portaOr4]);
+portaAndMeio4.addPortas([portaOr4]);
+portaAndBaixo4.addPortas([portaOr4]);
+
+
+
+portaXorBaixo1.addPortas([portaXorCima1]);
+
+portaXorBaixo2.addPortas([portaXorCima2]);
+
+portaXorBaixo3.addPortas([portaXorCima3]);
+
+portaXorBaixo4.addPortas([portaXorCima4]);
